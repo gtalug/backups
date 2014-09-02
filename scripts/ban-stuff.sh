b4babed9b9999b80340e1e1c97c2d979793a7474
Christopher Browne <cbbrowne@ca.afilias.info>
2014-09-02 16:12:30 -0400

    Script for fail2ban config

---
+++ scripts/ban-stuff.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ARCHIVEHOME=${HOME}/GTALUG/backups
+mkdir -p ${ARCHIVEHOME}
+cd ${ARCHIVEHOME}
+git fetch origin
+git pull origin master
+
+scp -r chris@penguin.gtalug.org:/etc/fail2ban ${ARCHIVEHOME}
+git add ${ARCHIVEHOME}
+git commit -m "scripts/backup-fail2ban-config.sh automated checkin"
+git push origin master
