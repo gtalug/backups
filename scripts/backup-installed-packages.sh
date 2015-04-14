635b234fec342ee9e64c9fd07f9f6e43440dbc0f
Alex Volkov <alex@flamy.ca>
2015-04-14 17:42:35 -0400

    Backing up installed packages script.

--- scripts/backup-installed-packages.sh
+++ scripts/backup-installed-packages.sh
@@ -10,7 +10,7 @@ cd ${ARCHIVEHOME}
 git fetch origin
 git pull origin master
 
-ssh ${USER}@penguin.gtalug.org "dpkg --list | grep ^ii | awk '{print $2,$3}'" > ${ARCHIVEHOME}/packages
+ssh ${USER}@penguin.gtalug.org "dpkg --list | grep ^ii | awk '{print \$2,\$3}'" > ${ARCHIVEHOME}/packages
 git add ${ARCHIVEHOME}/packages
-git commit -m "scripts/${0} system package list was modified"
-git push origin master
+#git commit -m "scripts/${0} system package list was modified"
+#git push origin master
