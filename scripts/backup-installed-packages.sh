810da4b47ee126bcca68abcb8d1e08fb5394f954
Alex Volkov <alex@flamy.ca>
2015-04-14 17:32:28 -0400

    Added script that backs up the list of package/versions installed on the system.

---
+++ scripts/backup-installed-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+##
+# Save the list of installed packages,versions
+##
+
+ARCHIVEHOME=${HOME}/GTALUG/backups
+mkdir -p ${ARCHIVEHOME}
+cd ${ARCHIVEHOME}
+git fetch origin
+git pull origin master
+
+ssh ${USER}@penguin.gtalug.org "dpkg --list | grep ^ii | awk '{print $2,$3}'" > ${ARCHIVEHOME}/packages
+git add ${ARCHIVEHOME}/packages
+git commit -m "scripts/${0} system package list was modified"
+git push origin master
