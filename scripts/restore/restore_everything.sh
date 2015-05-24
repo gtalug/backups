8e18d3205607d22d4be3bc9e3fc3156332257358
Alex Volkov <alex@flamy.ca>
2015-05-24 13:52:54 -0400

    restoring gtalug.org and board.gtalug.org.

--- scripts/restore/restore_everything.sh
+++ scripts/restore/restore_everything.sh
@@ -7,21 +7,28 @@
 restore_website(){
     WEBSITES_HOME=${HOME}/WebSites/
     RESTORE_DIR=${PWD}
-    sudo apt-get install -y git
+    declare -A websites
+    websites['org_gtalug_www']='https://github.com/gtalug/website.git'
+    websites['org_gtalug_board']='git@github.com:gtalug/board-meetings.git'
 
-    git clone https://github.com/gtalug/website.git
-
-    sudo apt-get install -y python-virtualenv node-less python-dev
-    for website in org_gtalug_www org_gtalug_board
+    sudo apt-get install -y git python-virtualenv node-less python-dev jekyll
+    for key in ${!websites[@]}
     do
-        mkdir -p ${WEBSITES_HOME}/${website}
-        cd ${WEBSITES_HOME}/${webiste}
+        mkdir -p ${WEBSITES_HOME}
+        git clone ${websites[${key}]}  ${WEBSITES_HOME}/${key}
+        cd ${WEBSITES_HOME}/${key}
         virtualenv env
         source env/bin/activate
-        [ -f requirements.txt ] && pip install -r requirements.txt
         pip install fabric
-        fab install
-        fab run
+        if [ -f requirements.txt ]
+        then
+            pip install -r requirements.txt
+            fab install
+            fab run &
+        else
+            fab jekyll
+        fi
+
     done
 }
 
