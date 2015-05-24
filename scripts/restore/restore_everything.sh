fd397e2460e4694225e99ab64e1cbd603ebbbcd0
Alex Volkov <alex@flamy.ca>
2015-05-24 14:17:37 -0400

    Installing board/gtalug dependencies.

--- scripts/restore/restore_everything.sh
+++ scripts/restore/restore_everything.sh
@@ -5,13 +5,14 @@
 ###
 
 restore_website(){
-    WEBSITES_HOME=${HOME}/WebSites/
+    WEBSITES_HOME=${HOME}/WebSites
     RESTORE_DIR=${PWD}
     declare -A websites
     websites['org_gtalug_www']='https://github.com/gtalug/website.git'
     websites['org_gtalug_board']='git@github.com:gtalug/board-meetings.git'
 
-    sudo apt-get install -y git python-virtualenv node-less python-dev jekyll
+    sudo apt-get install -y git python-virtualenv node-less python-dev \
+    libyaml-dev bundler jekyll
     for key in ${!websites[@]}
     do
         mkdir -p ${WEBSITES_HOME}
@@ -20,14 +21,17 @@ restore_website(){
         virtualenv env
         source env/bin/activate
         pip install fabric
+
         if [ -f requirements.txt ]
         then
-            pip install -r requirements.txt
             fab install
             fab run &
         else
-            fab jekyll
+            pip install PyYAML
+            fab build
+            fab run &
         fi
+        deactivate
 
     done
 }
